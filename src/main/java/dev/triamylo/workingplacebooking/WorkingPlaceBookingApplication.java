e6fcddea50c4c51ad3abffd78a059e6d5d241ccb
Triantafyllos Mylonas <triantafyllos.mylonas@schnellecke.com>
2024-01-05 15:58:54 +0100

    Add application setup and configuration files
    This commit introduces several new files that define the core setup and configuration for the WorkingPlaceBooking application. This includes the main application file and its corresponding tests, Maven wrapper scripts, project building files and startup configuration files. Each of these components ensures that the project infrastructure is correctly established and functioning, that it can be correctly compiled and built using Maven, and that the project is properly configured for development in different environments.

---
+++ src/main/java/dev/triamylo/workingplacebooking/WorkingPlaceBookingApplication.java
@@ -0,0 +1,13 @@
+package dev.triamylo.workingplacebooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkingPlaceBookingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorkingPlaceBookingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/triamylo/workingplacebooking/WorkingPlaceBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.triamylo.workingplacebooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkingPlaceBookingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
