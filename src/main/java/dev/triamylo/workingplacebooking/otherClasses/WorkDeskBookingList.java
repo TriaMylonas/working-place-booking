b74a92eb2a82592804fe12ee74597f3e2b68cb6b
Triantafyllos Mylonas <triantafyllos.mylonas@schnellecke.com>
2024-01-15 14:07:29 +0100

    Add WorkDeskBookingList.
    This new class contains properties related to desk bookings und is for the corresponding list that will be display in the frond end.

---
+++ src/main/java/dev/triamylo/workingplacebooking/otherClasses/WorkDeskBookingList.java
@@ -0,0 +1,26 @@
+package dev.triamylo.workingplacebooking.otherClasses;
+
+import dev.triamylo.workingplacebooking.model.Booking;
+import dev.triamylo.workingplacebooking.model.WorkDesk;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+import java.util.List;
+
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class WorkDeskBookingList {
+
+
+    private List<WorkDesk> workDesks;
+    private List<Booking> bookings;
+    private boolean workDeskAvailable;
+    private Date day;
+
+}
