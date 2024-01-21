5ec7418c717f45115929f3e65e6cedd4f5d8f0c9
TriaMylo <t.mylonas@outlook.de>
2024-01-21 16:34:35 +0100

    Disable security-related dependencies and configurations
    The security-related dependencies in the pom.xml file have been commented out. Also, the web security configuration and the first user configuration have been disabled by commenting out. These changes were necessary to address an issue in the security configurations. Further modifications and testing will be conducted for a permanent solution.

--- src/main/java/dev/triamylo/workingplacebooking/configuration/FirstUserConfig.java
+++ src/main/java/dev/triamylo/workingplacebooking/configuration/FirstUserConfig.java
@@ -1,68 +1,68 @@
-package dev.triamylo.workingplacebooking.configuration;
-
-import dev.triamylo.workingplacebooking.model.Role;
-import dev.triamylo.workingplacebooking.model.User;
-import dev.triamylo.workingplacebooking.repository.RoleRepository;
-import dev.triamylo.workingplacebooking.repository.UserRepository;
-import jakarta.annotation.PostConstruct;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.stereotype.Component;
-import org.springframework.transaction.support.TransactionTemplate;
-
-import java.util.Optional;
-
-@Component
-public class FirstUserConfig {
-    private final UserRepository userRepository;
-    private final RoleRepository roleRepository;
-    private final TransactionTemplate transaction;
-    private final PasswordEncoder passwordEncoder;
-
-    public FirstUserConfig(UserRepository userRepository, RoleRepository roleRepository,
-                           TransactionTemplate transaction, PasswordEncoder passwordEncoder) {
-        this.userRepository = userRepository;
-        this.roleRepository = roleRepository;
-        this.transaction = transaction;
-        this.passwordEncoder = passwordEncoder;
-    }
-
-    //    the PostConstruct annotation is used on a method that needs to be executed after
-    //    dependency injection is done to perform any initialization task!
-    @PostConstruct
-    public void initUser() {
-
-//        all of that must in one transaction with the database happen.
-//        first we look for the admin role, second check for the admin user.
-//        if they are not exist we make them. If they exist we use them.
-        transaction.execute(status -> {
-
-            Optional<Role> optionalAdminRole = roleRepository.findByName("ADMIN");
-
-            if(optionalAdminRole.isEmpty()){
-                Role adminRole = new Role("ADMIN", "administration role");
-                roleRepository.save(adminRole);
-                optionalAdminRole = roleRepository.findByName("ADMIN");
-            }
-
-            Optional<User> optionalAdminlUser = userRepository.findByUserName("adminUser");
-
-            if (optionalAdminlUser.isEmpty()) {
-                User adminUser = new User("adminUser", passwordEncoder.encode("admin"), "admin", "adminLastname", "admin@gmail.com");
-                userRepository.save(adminUser);
-                optionalAdminlUser = userRepository.findByUserName("adminUser");
-            }
-
-            // now that I have both I assign the role to the user and save him to the database
-            User adminUser = optionalAdminlUser.get();
-            Role adminRole = optionalAdminRole.get();
-
-            if(!adminUser.getRoles().contains(adminRole)){
-                adminUser.addRole(adminRole);
-                userRepository.save(adminUser);
-            }
-
-            return null;
-        });
-    }
-
-}
+//package dev.triamylo.workingplacebooking.configuration;
+//
+//import dev.triamylo.workingplacebooking.model.Role;
+//import dev.triamylo.workingplacebooking.model.User;
+//import dev.triamylo.workingplacebooking.repository.RoleRepository;
+//import dev.triamylo.workingplacebooking.repository.UserRepository;
+//import jakarta.annotation.PostConstruct;
+//import org.springframework.security.crypto.password.PasswordEncoder;
+//import org.springframework.stereotype.Component;
+//import org.springframework.transaction.support.TransactionTemplate;
+//
+//import java.util.Optional;
+//
+//@Component
+//public class FirstUserConfig {
+//    private final UserRepository userRepository;
+//    private final RoleRepository roleRepository;
+//    private final TransactionTemplate transaction;
+//    private final PasswordEncoder passwordEncoder;
+//
+//    public FirstUserConfig(UserRepository userRepository, RoleRepository roleRepository,
+//                           TransactionTemplate transaction, PasswordEncoder passwordEncoder) {
+//        this.userRepository = userRepository;
+//        this.roleRepository = roleRepository;
+//        this.transaction = transaction;
+//        this.passwordEncoder = passwordEncoder;
+//    }
+//
+//    //    the PostConstruct annotation is used on a method that needs to be executed after
+//    //    dependency injection is done to perform any initialization task!
+//    @PostConstruct
+//    public void initUser() {
+//
+////        all of that must in one transaction with the database happen.
+////        first we look for the admin role, second check for the admin user.
+////        if they are not exist we make them. If they exist we use them.
+//        transaction.execute(status -> {
+//
+//            Optional<Role> optionalAdminRole = roleRepository.findByName("ADMIN");
+//
+//            if(optionalAdminRole.isEmpty()){
+//                Role adminRole = new Role("ADMIN", "administration role");
+//                roleRepository.save(adminRole);
+//                optionalAdminRole = roleRepository.findByName("ADMIN");
+//            }
+//
+//            Optional<User> optionalAdminlUser = userRepository.findByUserName("adminUser");
+//
+//            if (optionalAdminlUser.isEmpty()) {
+//                User adminUser = new User("adminUser", passwordEncoder.encode("admin"), "admin", "adminLastname", "admin@gmail.com");
+//                userRepository.save(adminUser);
+//                optionalAdminlUser = userRepository.findByUserName("adminUser");
+//            }
+//
+//            // now that I have both I assign the role to the user and save him to the database
+//            User adminUser = optionalAdminlUser.get();
+//            Role adminRole = optionalAdminRole.get();
+//
+//            if(!adminUser.getRoles().contains(adminRole)){
+//                adminUser.addRole(adminRole);
+//                userRepository.save(adminUser);
+//            }
+//
+//            return null;
+//        });
+//    }
+//
+//}
--- src/main/java/dev/triamylo/workingplacebooking/configuration/WebSecurityConfig.java
+++ src/main/java/dev/triamylo/workingplacebooking/configuration/WebSecurityConfig.java
@@ -1,37 +1,37 @@
-package dev.triamylo.workingplacebooking.configuration;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractAuthenticationFilterConfigurer;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.SecurityFilterChain;
-
-@Configuration
-@EnableWebSecurity
-public class WebSecurityConfig {
-
-    @Bean
-    public PasswordEncoder encoder() {
-        return new BCryptPasswordEncoder();
-    }
-
-
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-
-        // SOS --> /images/** --> all unter one ordner so that they can authenticate with one's
-        //all can use the home page and can add new user, all the others musst be authenticated
-        http.authorizeHttpRequests((requests) -> requests
-                        .requestMatchers("/home", "/images/**").permitAll()
-                        .requestMatchers("/booking").hasRole("ADMIN") // die Liste kann von ADMIN ausgerufen und bearbeiten werden.
-                        .anyRequest()
-                        .authenticated())
-                .formLogin(AbstractAuthenticationFilterConfigurer::permitAll)  // das ist die default site from the security. (permitAll) I don't need to have permit to access in the login form.
-                .logout((logout) -> logout.logoutSuccessUrl("/").permitAll()); //(permitAll) I don't need to have permit to access in the logout form & goes to the start site again.
-        return http.build();
-
-    }
-}
+//package dev.triamylo.workingplacebooking.configuration;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configurers.AbstractAuthenticationFilterConfigurer;
+//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+//import org.springframework.security.crypto.password.PasswordEncoder;
+//import org.springframework.security.web.SecurityFilterChain;
+//
+//@Configuration
+//@EnableWebSecurity
+//public class WebSecurityConfig {
+//
+//    @Bean
+//    public PasswordEncoder encoder() {
+//        return new BCryptPasswordEncoder();
+//    }
+//
+//
+//    @Bean
+//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+//
+//        // SOS --> /images/** --> all unter one ordner so that they can authenticate with one's
+//        //all can use the home page and can add new user, all the others musst be authenticated
+//        http.authorizeHttpRequests((requests) -> requests
+//                        .requestMatchers("/home", "/images/**").permitAll()
+////                        .requestMatchers("/booking").hasRole("ADMIN") // die Liste kann von ADMIN ausgerufen und bearbeiten werden.
+//                        .anyRequest()
+//                        .authenticated())
+//                .formLogin(AbstractAuthenticationFilterConfigurer::permitAll)  // das ist die default site from the security. (permitAll) I don't need to have permit to access in the login form.
+//                .logout((logout) -> logout.logoutSuccessUrl("/home").permitAll()); //(permitAll) I don't need to have permit to access in the logout form & goes to the start site again.
+//        return http.build();
+//
+//    }
+//}
